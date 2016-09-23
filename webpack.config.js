cdad9402b0fa12539b90e3d3c4cd8be63f86b07c
Edwin Lin <edwinlin1987@gmail.com>
2016-09-22 23:33:27 -0700

    (init) Added webpack-validator for webpack config validation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: './build',
-    filename: 'bundle.js'
-  }
+const { resolve } = require('path');
+
+module.exports = env => {
+  return {
+    entry: './src/app.js',
+    output: {
+      path: resolve(__dirname,'dist'),
+      filename: 'bundle.js',
+      pathinfo: !env.prod
+    },
+    module: {
+      loaders: []
+    }
+  };
 };
