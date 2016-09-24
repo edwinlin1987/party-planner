3ea69049cf841457884e2b5493307ea74ec4a4bd
Edwin Lin <edwinlin1987@gmail.com>
2016-09-24 11:31:52 -0700

    (add) upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { resolve } = require('path');
 
 module.exports = env => {
+
+  const ifProd = value => env.prod ? value : undefined;
+  const removeEmpty = array => array.filter(i => !!i);
   return {
-    entry: './src/app.js',
+    entry: {
+      app: './app.js',
+      vendor: ['lodash']
+    },
     output: {
       path: resolve(__dirname,'dist'),
-      filename: 'bundle.js',
+      filename: 'bundle.[name].[chunkhash].js',
       pathinfo: !env.prod
     },
+    context: resolve(__dirname,'src'),
+    plugins: removeEmpty([
+      ifProd(new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      })),
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      })
+    ]), 
     module: {
       loaders: []
     }
