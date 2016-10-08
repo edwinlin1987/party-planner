7e0bbdcb05e19176f55081274a9c10a8a63408a4
Edwin Lin <edwinlin1987@gmail.com>
2016-10-07 23:48:24 -0700

    (add) prepared more application structure

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const initialState = window.__INITIAL_STATE__;
+
 const ROOT_NODE = document.getElementById('root');
 
 ReactDOM.render(<div>TESTING</div>, ROOT_NODE);
